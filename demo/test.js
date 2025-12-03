59ae443d79aa91eabff74d4f325e67207fbb4de3
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-12-03 19:20:52 +0330

    Change to default number, remove axious and use default js

--- demo/test.js
+++ demo/test.js
@@ -1,5 +1,6 @@
 const AsanakSms = require("soap-asanak-sms");
-const sms = new AsanakSms({});
+
+const sms = new AsanakSms({ debug: true, });
 
 (async () => {
   await sms.send("989121111010", "سلام - این یک پیام تست از npm package");
