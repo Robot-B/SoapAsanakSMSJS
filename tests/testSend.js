3f95b9e602029a1eabb67e183476845a0ac6bb0d
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-12-03 18:40:46 +0330

    Init lib and create a test script

---
+++ tests/testSend.js
@@ -0,0 +1,12 @@
+const AsanakSms = require("../lib/AsanakSms");
+
+(async () => {
+  const sms = new AsanakSms({});
+  
+  try {
+    const response = await sms.send("989121111010", "سلام - این یک پیام تست است");
+    console.log("Response received successfully!");
+  } catch (err) {
+    console.error("Failed to send SMS:", err.message);
+  }
+})();
